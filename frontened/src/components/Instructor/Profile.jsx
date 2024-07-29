5711b8b380280fa7a142b553598ed27972204966
Mahir Hayaat <mahirhayat4u@gmail.com>
2024-07-30 00:55:59 +0530

    frontend

---
+++ frontened/src/components/Instructor/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Profile = () => {
+  return (
+    <div> 
+    <Link to="/createTodo">
+      create todo
+    </Link>
+    
+    </div>
+  )
+}
+
+export default Profile
---
+++ frontened/src/components/Student/ProfileStudent.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+ 
+import { Link  } from 'react-router-dom'
+ 
+
+const ProfileStudent = () => {
+ 
+  return (
+    <div>
+
+ 
+     <Link to="/createTodo">
+      create todo
+    </Link>
+    
+    </div>
+  )
+}
+
+export default ProfileStudent
